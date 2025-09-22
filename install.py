d71906158e76b55debe0f1368a3653ddf00f81fd
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2025-09-22 12:09:59 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,7 @@
 import os
 
 def main():  
+    os.system("python.exe -m pip install --upgrade pip")
     modules = ["ttkbootstrap", "mediapipe", "opencv-python", "numpy", "pynput", "torch"]  
     print("preparing to install {}".format(modules))
     for i in range(0, len(modules)):
