4076853fd41bbb9600bac2ad29b9006cb8dbf9ab
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-11-03 16:49:29 +0800

    Update install.py

--- Extension_modules/install.py
+++ Extension_modules/install.py
@@ -2,7 +2,7 @@ import os
 
 def main():  
     os.system("python.exe -m pip install --upgrade pip")
-    modules = ["ttkbootstrap", "mediapipe", "opencv-python", "numpy", "pynput"]  
+    modules = ["ttkbootstrap", "mediapipe", "opencv-python", "opencv-contrib-python", "numpy", "pynput"]  
     print("preparing to install {}".format(modules))
     for i in range(0, len(modules)):
         os.system("pip install "+modules[i])
