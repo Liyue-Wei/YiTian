1d65bbf015944393b3224dd22e6fb8e6f88c6e58
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-11-20 16:29:49 +0800

    Update install.py

--- install.py
+++ install.py
@@ -9,13 +9,20 @@ packages = ["ttkbootstrap",
             "pynput"]
 
 def installer(package):
-    pass
+    print(f"[+] Installing {package}")
+    try:
+        pass
+    except:
+        pass
 
 def main():
-    print(f"Currient version = Python {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")
+    print(f"Currient version: Python {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")
     if sys.version_info < (3, 10):
         print(f"Error: Python 3.10 or higher is required. Terminated...")
         sys.exit()
+
+    for package in packages:
+        installer(package)
     
 if __name__ == "__main__":
     main()
