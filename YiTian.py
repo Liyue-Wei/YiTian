64eb5c3e7ffe23bb31be4e7cacacaf85559549c1
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-11-22 02:05:57 +0800

    Update YiTian.py

--- YiTian.py
+++ YiTian.py
@@ -5,3 +5,6 @@ YiTian - Touch Typing Correction System
 Copyright (c) 2025 Zhang Zhewei (Liyue-Wei)
 Licensed under the GNU GPL v3.0 License. 
 '''
+
+from extmodules import keyboard_listener as kl
+from extmodules import hand_detector as hd
