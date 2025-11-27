a0edeea6ad8303d19e95a9ff46b93bc42d9b3181
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-11-28 02:28:40 +0800

    Update shm_cfg.py

---
+++ extmodules/shm_cfg.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''
+YiTian - Share Memory Configuration
+
+Copyright (c) 2025 Zhang Zhewei (Liyue-Wei)
+Licensed under the GNU GPL v3.0 License. 
+'''
+
+SHM_FRAME_ID = "YiTian_SHM_Frame"
+SHM_RESULT_ID = "YiTian_SHM_RESULT"
+
+WIDTH = 1920
+HEIGHT = 1080
+CHANNELS = 3
+FRAME_SIZE = WIDTH * HEIGHT * CHANNELS
+
+RESULT_SIZE = 1024
