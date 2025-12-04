6d5b5d065bc045d5f182413654e49b0baa8aa5c9
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-12-05 01:56:48 +0800

    Update hand_detector.py

--- extmodules/hand_detector.py
+++ extmodules/hand_detector.py
@@ -14,5 +14,5 @@ class HandDetector:
     def __init__(self):
         try:
             shm_frame = shared_memory.SharedMemory(name=shm_cfg.SHM_FRAME_ID)
-        except:
-            raise IOError(f"Shared Memory unavaliable: {shm_cfg.SHM_FRAME_ID}")
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Shared Memory unavaliable: {shm_cfg.SHM_FRAME_ID}")
