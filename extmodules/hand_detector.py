f24c747b62eefd5244b412723e9388adb31442ba
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-12-05 10:48:46 +0800

    Update hand_detector.py

--- extmodules/hand_detector.py
+++ extmodules/hand_detector.py
@@ -16,17 +16,24 @@ class HandDetector:
         self.shm_result = None
 
         try:
-            shm_frame = shared_memory.SharedMemory(name=shm_cfg.SHM_FRAME_ID)
+            self.shm_frame = shared_memory.SharedMemory(name=shm_cfg.SHM_FRAME_ID)
         except FileNotFoundError:
             raise FileNotFoundError(f"Shared Memory unavaliable: {shm_cfg.SHM_FRAME_ID}")
         
         try:
-            shm_result = shared_memory.SharedMemory(create=True, size=shm_cfg.RESULT_SIZE, name=shm_cfg.SHM_RESULT_ID)
+            self.shm_result = shared_memory.SharedMemory(create=True, size=shm_cfg.RESULT_SIZE, name=shm_cfg.SHM_RESULT_ID)
         except FileExistsError:
             print("Process: Shared Memory already exists. Cleaning up...")
-            with shared_memory.SharedMemory(name=shm_cfg.SHM_RESULT_ID) as temp_shm:
-                temp_shm.unlink()
-            shm_result = shared_memory.SharedMemory(create=True, size=shm_cfg.RESULT_SIZE, name=shm_cfg.SHM_RESULT_ID)
+            try:
+                with shared_memory.SharedMemory(name=shm_cfg.SHM_RESULT_ID) as temp_shm:
+                    temp_shm.unlink()
+            except FileNotFoundError:
+                pass
+            except Exception as e:
+                self.cleanup()
+                raise RuntimeError(f"Unexpected error occurred: {e}")
+            
+            self.shm_result = shared_memory.SharedMemory(create=True, size=shm_cfg.RESULT_SIZE, name=shm_cfg.SHM_RESULT_ID)
 
     def cleanup(self):
         if self.shm_frame:
