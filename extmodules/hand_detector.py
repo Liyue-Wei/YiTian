4313002bf6d686774acb7573fc5af2cdc5ab9756
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-12-05 10:32:42 +0800

    Update hand_detector.py

--- extmodules/hand_detector.py
+++ extmodules/hand_detector.py
@@ -12,7 +12,21 @@ import cv2
 
 class HandDetector:
     def __init__(self):
+        self.shm_frame = None
+        self.shm_result = None
+
         try:
             shm_frame = shared_memory.SharedMemory(name=shm_cfg.SHM_FRAME_ID)
         except FileNotFoundError:
             raise FileNotFoundError(f"Shared Memory unavaliable: {shm_cfg.SHM_FRAME_ID}")
+        
+        try:
+            shm_result = shared_memory.SharedMemory(create=True, size=shm_cfg.RESULT_SIZE, name=shm_cfg.SHM_RESULT_ID)
+        except FileExistsError:
+            print("Process: Shared Memory already exists. Cleaning up...")
+            with shared_memory.SharedMemory(name=shm_cfg.SHM_RESULT_ID) as temp_shm:
+                temp_shm.unlink()
+            shm_result = shared_memory.SharedMemory(create=True, size=shm_cfg.RESULT_SIZE, name=shm_cfg.SHM_RESULT_ID)
+
+    def cleanup():
+        pass
