5f630b7ad4cab1273137e63e08b40695fc781611
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-12-15 22:17:03 +0800

    Update YiTian_OLD.py

--- YiTian_OLD.py
+++ YiTian_OLD.py
@@ -208,7 +208,7 @@ class TypingCorrector:
 # 測試代碼(保持註解狀態)
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
     cap.set(cv2.CAP_PROP_FPS, 60)
