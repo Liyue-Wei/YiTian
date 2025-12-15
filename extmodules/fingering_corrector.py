a2163aa31b1d047f15a38fcb9dea0fae6423af47
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-12-15 17:16:36 +0800

    Update fingering_corrector.py

--- extmodules/fingering_corrector.py
+++ extmodules/fingering_corrector.py
@@ -5,3 +5,8 @@ YiTian - Fingering Corrector Module
 Copyright (c) 2025 Zhang Zhewei (Liyue-Wei)
 Licensed under the GNU GPL v3.0 License. 
 '''
+
+from multiprocessing import shared_memory
+import numpy as np
+import shm_cfg
+import cv2
