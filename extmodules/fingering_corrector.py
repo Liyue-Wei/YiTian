c18a1cd828cfa550d14d030696062d0c27f872a7
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-12-18 01:12:27 +0800

    Update fingering_corrector.py

--- extmodules/fingering_corrector.py
+++ extmodules/fingering_corrector.py
@@ -18,5 +18,27 @@ class FingeringCorrector:
         self.WIDTH = shm_cfg.WIDTH
         self.HEIGHT = shm_cfg.HEIGHT
 
+        self.key_layout = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+        self.key_map = {}
+
+        self.finger_map = {
+            'q': 'LEFT_PINKY', 'a': 'LEFT_PINKY', 'z': 'LEFT_PINKY', 
+            'w': 'LEFT_RING', 's': 'LEFT_RING', 'x': 'LEFT_RING',
+            'e': 'LEFT_MIDDLE', 'd': 'LEFT_MIDDLE', 'c': 'LEFT_MIDDLE', 
+            'r': 'LEFT_INDEX', 'f': 'LEFT_INDEX', 'v': 'LEFT_INDEX',
+            't': 'LEFT_INDEX', 'g': 'LEFT_INDEX', 'b': 'LEFT_INDEX', 
+            
+            'y': 'RIGHT_INDEX', 'h': 'RIGHT_INDEX', 'n': 'RIGHT_INDEX',
+            'u': 'RIGHT_INDEX', 'j': 'RIGHT_INDEX', 'm': 'RIGHT_INDEX', 
+            'i': 'RIGHT_MIDDLE', 'k': 'RIGHT_MIDDLE', 
+            'o': 'RIGHT_RING', 'l': 'RIGHT_RING', 
+            'p': 'RIGHT_PINKY',
+        }
+        
+        self.fingertip_indices = {
+            "RIGHT_THUMB": 4, "RIGHT_INDEX": 8, "RIGHT_MIDDLE": 12, "RIGHT_RING": 16, "RIGHT_PINKY": 20,
+            "LEFT_THUMB": 4, "LEFT_INDEX": 8, "LEFT_MIDDLE": 12, "LEFT_RING": 16, "LEFT_PINKY": 20
+        }        
+
     def key_map_calibration(self):
         anchor = ['q', 'p', 'z', 'm']
